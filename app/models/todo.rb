3c78085690db49193487c0d26bbd92c6ca4c5665
cloudy9101 <cloudy9101@gmail.com>
2016-06-24 23:27:04 +0800

    Add model relations

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,8 @@
 class Todo < ActiveRecord::Base
+  belongs_to :team
+  belongs_to :user
+  belongs_to :project
+  belongs_to :assigned_user, class_name: 'User'
+
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  belongs_to :team
+
+  has_many :todos
+  has_many :accesses
+
+  has_many :assigned_todos, class_name: 'Todo'
 end
